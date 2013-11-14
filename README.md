66e3d857fe0c11a56e48eb9e43e1ede2aaf7f1ca
Ariel Orozco <aorozco@77digital.com>
2013-11-14 16:52:14 -0600

    Updated code in update-server.php

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-web-hooks
-=========
+#GitHub WebHooks
 
-Web hooks to use with Github
+##Introduction
+Various WebHooks to use on a remote server.
+
+##Installation
+
+1) Clone the repo: `git clone https://github.com/arielcr/web-hooks.git`
+2) Upload the web-hooks folder to your site root
+
+##Usage
+
+#### Update Server WebHook
+WebHook that updates the files on a remote server every time your make a push to GitHub
+
+1) Go to `[Your Repo]->[Settings]->[Service Hooks]->[WebHook URLs]`
+2) Place the URL to `http://yoursite.com/web-hooks/update-server.php`
+3) Commit/Push your site
+4) Your site will be under `http://yoursite.com/your-repo-name`
+
+More WebHooks to be added soon ...
--- update-server.php
+++ update-server.php
@@ -1,22 +1,26 @@
 <?php
 
+date_default_timezone_set('America/Costa_Rica');
+
 require 'vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Symfony\Component\Yaml\Yaml;
 
+$config = Yaml::parse(file_get_contents('config.yml'));
 $payload = json_decode($_POST['payload']);
 
-$message = print_r($payload, true);
+$LOCAL_REPO = $_SERVER['DOCUMENT_ROOT'] . "/" . $payload->repository->name;
+$REMOTE_REPO = $payload->repository->url . ".git";
 
-// create a log channel
-$log = new Logger('updates');
-$log->pushHandler(new StreamHandler('logs/updates.log', Logger::INFO));
+if (file_exists($LOCAL_REPO)) {
+    //shell_exec("rm -rf {$LOCAL_REPO}");
+    shell_exec("cd {$LOCAL_REPO} && git fetch --all && git reset --hard origin/master");
+} else {
+    shell_exec("cd {$_SERVER['DOCUMENT_ROOT']} && git clone {$REMOTE_REPO} ");
+}
 
-// add records to the log
-$log->addInfo($payload->commits[0]->message);
-
-
-mail("bassplayer85@gmail.com", "TEST HOOKS 2", $message);
-
+$log = new Logger('UPDATES');
+$log->pushHandler(new StreamHandler('logs/' . $payload->repository->name . '.log', Logger::INFO));
+$log->addInfo(substr($payload->commits[0]->id, 0, 8) . " " . $payload->commits[0]->message);
