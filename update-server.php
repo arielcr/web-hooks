f791a10f109c856b04975a35fb51bd8c4afe730c
Ariel Orozco <aorozco@77digital.com>
2013-11-14 17:02:23 -0600

    Updated code in update-server.php

--- update-server.php
+++ update-server.php
@@ -15,7 +15,6 @@ $LOCAL_REPO = $_SERVER['DOCUMENT_ROOT'] . "/" . $payload->repository->name;
 $REMOTE_REPO = $payload->repository->url . ".git";
 
 if (file_exists($LOCAL_REPO)) {
-    //shell_exec("rm -rf {$LOCAL_REPO}");
     shell_exec("cd {$LOCAL_REPO} && git fetch --all && git reset --hard origin/master");
 } else {
     shell_exec("cd {$_SERVER['DOCUMENT_ROOT']} && git clone {$REMOTE_REPO} ");
